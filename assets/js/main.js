175ae62e95fd49dd6fd48b6f801d9c99d05f24a7
Ezekiel Ogunniwa <ogunniwaezekiel@gmail.com>
2022-06-20 18:44:39 +0100

    landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,30 @@ sideMenu.style.display="none";
 menuBtn.style.display="block";
 closeBtn.style.display="none";
 }
+
+// Owl Carousel
+
+$('.owl-carousel').owlCarousel({
+loop:false,
+margin:20,
+nav:true,
+dots:false,
+autoplay:true,
+autoplayTimeout:1500,
+autoplayHoverPause:true,
+stagePadding:50,
+responsive:{
+  0:{
+      items:2,
+      center:true
+  },
+  600:{
+      items:3
+     
+  },
+  1000:{
+      items:6,
+      nav:true
+  }
+}
+})
