56b41ae309a484a0935532547034e3af7d9ec7ba
Ezekiel Ogunniwa <ogunniwaezekiel@gmail.com>
2022-06-22 18:22:42 +0100

    adding shop and sproduct page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
+// ====== Side Menu on index.html ======
 let sideSubmenu = document.getElementById("side-submenu");
 let menuBtn = document.getElementById("menu-btn");
 let closeBtn = document.getElementById("close-btn");
-
-
 
 openmenu = () => {
 sideSubmenu.style.display="block";
@@ -16,3 +15,23 @@ menuBtn.style.display="block";
 closeBtn.style.display="none";
 }
 
+// ====== Image selection on sproduct.html ======
+
+var mainImg = document.getElementById("main-img");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function() {
+  mainImg.src = smallImg[0].src;
+}
+
+smallImg[1].onclick = function() {
+  mainImg.src = smallImg[1].src;
+}
+
+smallImg[2].onclick = function() {
+  mainImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = function() {
+  mainImg.src = smallImg[3].src;
+}
