79f4c0263b6925832cdad2ad3149eb020e44c8e4
Ezekiel Ogunniwa <ogunniwaezekiel@gmail.com>
2022-06-24 19:10:29 +0100

    commit search bare

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,26 @@ menuBtn.style.display="block";
 closeBtn.style.display="none";
 }
 
+// ====== Search select in index.html ======
+
+let select = document.getElementById("select");
+let list = document.getElementById("list");
+let selectText = document.getElementById("selectText");
+let inputfield = document.getElementById("inputfield");
+let options = document.getElementsByClassName("options");
+
+select.onclick = function() {
+  list.classList.toggle("open");
+}
+
+for (option of options) {
+  option.onclick = function(){
+    selectText.innerHTML = this.innerHTML;
+    inputfield.placeholder = `Search in ${selectText.innerHTML}`
+  }
+}
+
+
 // ====== Image selection on sproduct.html ======
 
 var mainImg = document.getElementById("main-img");
