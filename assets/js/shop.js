94bcfa473a66617ff0df211b8e17c269d5da21b1
Ezekiel Ogunniwa <ogunniwaezekiel@gmail.com>
2022-07-12 16:44:54 +0100

    commit

---
+++ assets/js/shop.js
@@ -0,0 +1,26 @@
+// ====== List Grid Vie Toggle on shop.html ======
+
+var li_links = document.querySelectorAll('.links ul li');
+var view_wraps = document.querySelectorAll('.view_wrap');
+
+li_links.forEach((link) => {
+  link.addEventListener('click', () => {
+    li_links.forEach((item) => {
+      item.classList.remove('active')
+    })
+
+    link.classList.add('active');
+
+    var li_view = link.getAttribute('data-view');
+
+    view_wraps.forEach((view) => {
+      view.style.display = 'none';
+    })
+
+    if(li_view == 'list-view') {
+      document.querySelector('.' + li_view).style.display = 'block';
+    } else {
+      document.querySelector('.' + li_view).style.display = 'block';
+    }
+  })
+})
